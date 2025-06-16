b46d01e6c1d5a8b52edc92c2b749b7b4101d6cb8
Andy Kayley <github@kayley.name>
2025-06-17 00:02:36 +0100

    Start Implementing Generator

--- MockGenerating/Sources/MockGenerating/Generator/Generator.swift
+++ MockGenerating/Sources/MockGenerating/Generator/Generator.swift
@@ -32,18 +32,17 @@ public class Generator {
 
     @discardableResult
     public func generate() -> String {
-//        let presenter = MockViewPresenter(view: view)
-//        setScope(for: presenter)
-//        presenter.setClassInitializers(getClassInitializersRemovingDuplicates())
-//        presenter.addClassProperties(getClassPropertiesRemovingDuplicates())
-//        presenter.addClassMethods(getClassMethodsRemovingDuplicates())
-//        presenter.addClassSubscripts(getClassSubscriptsRemovingDuplicates())
-//        presenter.addInitializers(getInitializersRemovingDuplicates())
-//        presenter.addProperties(getPropertiesRemovingDuplicates())
-//        presenter.addMethods(getMethodsRemovingDuplicates())
-//        presenter.addSubscripts(getSubscriptsRemovingDuplicates())
-//        return presenter.generate()
-        return ""
+        let presenter = MockViewPresenter(view: view)
+        setScope(for: presenter)
+        presenter.set(classInitializers: getClassInitializersRemovingDuplicates())
+        presenter.add(classProperties: getClassPropertiesRemovingDuplicates())
+        presenter.add(classMethods: getClassMethodsRemovingDuplicates())
+        presenter.add(classSubscripts: getClassSubscriptsRemovingDuplicates())
+        presenter.add(initializers: getInitializersRemovingDuplicates())
+        presenter.add(properties: getPropertiesRemovingDuplicates())
+        presenter.add(methods: getMethodsRemovingDuplicates())
+        presenter.add(subscripts: getSubscriptsRemovingDuplicates())
+        return presenter.generate()
     }
 
     private func setScope(for presenter: MockViewPresenter) {
@@ -56,39 +55,44 @@ public class Generator {
         return classes.flatMap { $0.initializers }
     }
 
-//    private func getClassPropertiesRemovingDuplicates() -> [Property] {
+    private func getClassPropertiesRemovingDuplicates() -> [Property] {
+        []
 //        return Array(Set(
 //            classes.flatMap { $0.properties }.map { SignatureGenerator.signature($0) }
 //        )).compactMap { signature in
 //            classes.flatMap { $0.properties }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
-//
-//    private func getClassMethodsRemovingDuplicates() -> [Method] {
+    }
+
+    private func getClassMethodsRemovingDuplicates() -> [Method] {
+        []
 //        return Array(Set(
 //            classes.flatMap { $0.methods }.map { SignatureGenerator.signature($0) }
 //        )).compactMap { signature in
 //            classes.flatMap { $0.methods }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
-//
-//    private func getClassSubscriptsRemovingDuplicates() -> [Subscript] {
+    }
+
+    private func getClassSubscriptsRemovingDuplicates() -> [Subscript] {
+        []
 //        return Array(Set(
 //            classes.flatMap { $0.subscripts }.map { SignatureGenerator.signature($0) }
 //        )).compactMap { signature in
 //            classes.flatMap { $0.subscripts }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
-//
-//    private func getInitializersRemovingDuplicates() -> [Initializer] {
+    }
+
+    private func getInitializersRemovingDuplicates() -> [Initializer] {
+        []
 //        return Array(Set(
 //            protocols.flatMap { $0.initializers }.map { SignatureGenerator.signature($0) }
 //        )).compactMap { signature in
 //            protocols.flatMap { $0.initializers }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
-//
-//    private func getPropertiesRemovingDuplicates() -> [Property] {
+    }
+
+    private func getPropertiesRemovingDuplicates() -> [Property] {
+        []
 //        let classSignatures = Set(classes.flatMap { $0.properties }.map { SignatureGenerator.signature($0) })
 //        return Array(Set(
 //            protocols.flatMap { $0.properties }
@@ -97,9 +101,10 @@ public class Generator {
 //        )).compactMap { signature in
 //            protocols.flatMap { $0.properties }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
-//
-//    private func getMethodsRemovingDuplicates() -> [Method] {
+    }
+
+    private func getMethodsRemovingDuplicates() -> [Method] {
+        []
 //        let classSignatures = Set(classes.flatMap { $0.methods }.map { SignatureGenerator.signature($0) })
 //        return Array(Set(
 //            protocols.flatMap { $0.methods }
@@ -108,9 +113,10 @@ public class Generator {
 //        )).compactMap { signature in
 //            protocols.flatMap { $0.methods }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
-//
-//    private func getSubscriptsRemovingDuplicates() -> [Subscript] {
+    }
+
+    private func getSubscriptsRemovingDuplicates() -> [Subscript] {
+        []
 //        let classSignatures = Set(classes.flatMap { $0.subscripts }.map { SignatureGenerator.signature($0) })
 //        return Array(Set(
 //            protocols.flatMap { $0.subscripts }
@@ -119,5 +125,5 @@ public class Generator {
 //        )).compactMap { signature in
 //            protocols.flatMap { $0.subscripts }.first { SignatureGenerator.signature($0) == signature }
 //        }
-//    }
+    }
 }
